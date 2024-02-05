6d20fcc63a4e61692a0de558dcb56715755bbc99
Toufiqul <fahim.prime@gmail.com>
2024-02-05 10:23:56 +0600

    feat: create get and post store api endpoint

--- restApi/app.py
+++ restApi/app.py
@@ -1,3 +1,34 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+stores = [
+    {
+        "name":"generic store",
+        "items":[
+            {
+                "name":"apple",
+                "price":10
+            },
+            {
+                "name":"banana",
+                "price":20
+            }
+        ]
+    }
+]
+
+@app.get("/store")  #http://127.0.0.1:5000/store
+def get_store():
+    return {"stores":stores}
+
+@app.post("/store") #http://127.0.0.1:5000/store
+def create_store():
+    request_data = request.get_json() #request is the body sent in the post request. comes from flask. import from flask
+
+    new_store = {
+        "name":request_data["name"],
+        "items":request_data["items"]
+    }
+    stores.append(new_store)
+    return new_store,201 #201 is the return status code
