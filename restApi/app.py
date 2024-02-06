3dc59fdd56f8464b54f7094770bce77ad3ed83cc
Toufiqul <fahim.prime@gmail.com>
2024-02-06 17:28:15 +0600

    fix: swagger-ui showing
    had forgotten api = Api(app)
    
    api.register_blueprint(ItemBlueprint)
    api.register_blueprint(StoreBlueprint) in app.py

--- restApi/app.py
+++ restApi/app.py
@@ -14,6 +14,10 @@ app.config["OPENAPI_URL_PREFIX"] = "/"
 app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
 app.config["OPENAPI_SWAGGER_UI_URL"]="https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
+api = Api(app)
+
+api.register_blueprint(ItemBlueprint)
+api.register_blueprint(StoreBlueprint)
 
 # sudo docker build -t flask-smorest-api .
 
