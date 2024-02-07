b7b81775a2db7bebc2ef5a525feb715b40882780
Toufiqul <fahim.prime@gmail.com>
2024-02-07 12:19:31 +0600

    error:

--- restApi/app.py
+++ restApi/app.py
@@ -1,24 +1,38 @@
+import os
 from flask import Flask
 from flask_smorest import Api
+
+from db import db
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 
-app = Flask(__name__)
+import models
 
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "Stores rest API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"]="https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+def create_app(db_url=None):
+    app = Flask(__name__)
 
-api = Api(app)
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores rest API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"]="https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"]=db_url or os.getenv("DATABASE_URL" ,"sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
 
-api.register_blueprint(ItemBlueprint)
-api.register_blueprint(StoreBlueprint)
+    
+    api = Api(app)
 
+    def create_tables():
+        db.create_all()
+
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+    
+    return app
 # sudo docker build -t flask-smorest-api .
 
 # sudo docker run -p 5005:5000 -w /app -v "$(pwd):/app" flask-smorest-api
--- restApi/models/item.py
+++ restApi/models/item.py
@@ -1,6 +1,6 @@
 from db import db
 
-class ItemModle(db.Model):
+class ItemModel(db.Model):
     __tablename__ = "items"
 
     id = db.Column(db.Integer, primary_key=True)
