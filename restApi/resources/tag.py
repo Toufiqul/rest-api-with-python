6c39f8cb49b6c2e0742a5975ffc475f46f50f9be
Toufiqul <fahim.prime@gmail.com>
2024-02-08 13:58:27 +0600

    many to many relationship with items and tags

--- restApi/resources/tag.py
+++ restApi/resources/tag.py
@@ -1,9 +1,9 @@
 from db import db
-from models import StoreModel,TagModel
+from models import StoreModel,TagModel,ItemModel
 from schemas import TagSchema
 from flask_smorest import Blueprint, abort
 from flask.views import MethodView
-from schemas import TagSchema
+from schemas import TagSchema,TagAndItemSchema
 from sqlalchemy.exc import SQLAlchemyError
 
 blp = Blueprint("Tags","tags",description="Operations on tags")
@@ -25,9 +25,65 @@ class Tag(MethodView):
         except SQLAlchemyError as e:
             abort(500, message=str(e))
 
-    @blp.route("/tag/<string:tag_id>")
-    class Tag(MethodView):
-        @blp.response(200, TagSchema)
-        def get(self, tag_id):
-            tag = TagModel.query.get_or_404(tag_id)
-            return tag
+@blp.route("/tag/<string:tag_id>")
+class Tag(MethodView):
+    @blp.response(200, TagSchema)
+    def get(self, tag_id):
+        tag = TagModel.query.get_or_404(tag_id)
+        return tag
+    
+    @blp.response(
+        202,
+        description="deletes a tab if no item is tagged with the given tag"
+        example={"message": "tag deleted"}
+    )
+    @blp.alt_response(
+        404,
+        description="tag not found"
+    )
+    @blp.alt_response(
+        400, 
+        description="returned if tag has item tagged with it"
+    )
+    def delete(self, tag_id):
+        tag=TagModel.query.get_or_404(tag_id)
+
+        if not tag.items:
+            db.session.delete(tag)
+            db.session.commit()
+            return {"message": "tag deleted"}
+        abort(
+            400, 
+            message="Tag could not be deleted. it has items linked to it"
+            )
+        
+@blp.route("/item/<string:item_id>/tag/<string:tag_id>")
+class Tag(MethodView):
+    @blp.response(201, TagSchema)
+    def ger(self,item_id,tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+
+        item.tags.append(tag)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            abort(500, message=str(e))
+        return tag
+    
+    @blp.response(201, TagAndItemSchema)
+    def ger(self,item_id,tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+
+        item.tags.remove(tag)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            abort(500, message=str(e))
+
+        return {"message": "item removed from database"}
